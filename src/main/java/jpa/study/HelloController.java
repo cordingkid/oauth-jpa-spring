dc8b6e79b315acdc8481e7e29e6fd7e13292a31a
codingkid <jjg1014@naver.com>
2024-04-24 14:36:38 +0900

    feat: resolver 동작 확인을 위한 api 추가

--- src/main/java/jpa/study/HelloController.java
+++ src/main/java/jpa/study/HelloController.java
@@ -1,13 +1,22 @@
 package jpa.study;
 
+import jpa.study.common.auth.Auth;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
         return "Hello, mfk";
+    }
+
+    @GetMapping("/hello-auth")
+    public String helloAuth(@Auth Long userId) {
+        log.info("userId = " + userId);
+        return "Hello, Auth: " + userId;
     }
 }

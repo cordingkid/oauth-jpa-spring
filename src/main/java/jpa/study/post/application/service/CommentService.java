cfcfdbf82aa6ea04b1aad1e667f5694ed4e809de
codingkid <jjg1014@naver.com>
2024-06-03 22:20:57 +0900

    feat: replyRepository 추가

--- src/main/java/jpa/study/post/application/service/CommentService.java
+++ src/main/java/jpa/study/post/application/service/CommentService.java
@@ -6,6 +6,7 @@ import jpa.study.post.presentation.dto.CommentUpdateRequest;
 import jpa.study.post.presentation.dto.CommentWriteRequest;
 import jpa.study.post.repository.CommentRepository;
 import jpa.study.post.repository.PostRepository;
+import jpa.study.post.repository.ReplyRepository;
 import jpa.study.user.application.domain.User;
 import jpa.study.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
     private final PostRepository postRepository;
+    private final ReplyRepository replyRepository;
 
     public Long writeComment(Long userId, Long postId, CommentWriteRequest request) {
         User user = userRepository.getById(userId);

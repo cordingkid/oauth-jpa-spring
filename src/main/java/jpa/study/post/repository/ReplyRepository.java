8ea82a3534eabc4f24889d8f74eb15abcc73ddd5
codingkid <jjg1014@naver.com>
2024-06-04 17:45:15 +0900

    feat: getById 재정의

--- src/main/java/jpa/study/post/repository/ReplyRepository.java
+++ src/main/java/jpa/study/post/repository/ReplyRepository.java
@@ -1,7 +1,16 @@
 package jpa.study.post.repository;
 
+import jpa.study.common.exception.CustomException;
+import jpa.study.post.application.domain.Post;
 import jpa.study.post.application.domain.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import static jpa.study.common.exception.ErrorCode.RESOURCE_NOT_FOUND;
+
 public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    default Reply getById(Long replyId){
+        return findById(replyId)
+                .orElseThrow(() -> new CustomException(RESOURCE_NOT_FOUND));
+    }
 }

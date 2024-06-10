927794c3d5a687b1cfce79e5689820ede44e37b5
codingkid <jjg1014@naver.com>
2024-06-10 21:54:49 +0900

    feat: CommentController 추가

---
+++ src/main/java/jpa/study/post/presentation/contoller/CommentController.java
@@ -0,0 +1,14 @@
+package jpa.study.post.presentation.contoller;
+
+import jpa.study.post.application.service.CommentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+}

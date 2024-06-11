3e962c44b4161510d95134f451fc32c46c7a4641
codingkid <jjg1014@naver.com>
2024-06-11 21:46:57 +0900

    feat: post

--- src/main/java/jpa/study/post/presentation/contoller/PostController.java
+++ src/main/java/jpa/study/post/presentation/contoller/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,4 +22,9 @@ public class PostController {
         PostsResponse response = postService.getPostList(pageable);
         return ResponseEntity.ok().body(response);
     }
+
+//    @PostMapping("/post")
+//    public ResponseEntity<Void> addPost() {
+//        return null;
+//    }
 }
